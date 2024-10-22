d8fd36cf1df2cfebdce7ed04d4237c8ba6ad4353
fllovfgv <fllovfgv@gmail.com>
2024-10-23 08:38:02 +0900

    Update dependencies: Add google-cloud-aiplatform

--- app_genaiwk/util.py
+++ app_genaiwk/util.py
@@ -1,8 +1,25 @@
-
 import requests
 from typing import List, Dict, Any, Union
 from googleapiclient.discovery import build
 from bs4 import BeautifulSoup
+
+import vertexai
+from vertexai.preview.generative_models import GenerativeModel, Part, Image, Content
+from vertexai.preview import generative_models
+
+# geminiのモデル名
+model_gemini_pro10 = "gemini-1.0-pro-002"
+model_gemini_pro15 = "gemini-1.5-pro-002"
+model_gemini_pro15_flash = "gemini-1.5-flash-002"
+
+# テキストのセーフティ設定は最低にしておく（デモのため）
+safety_settings_NONE = {
+    generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_NONE,
+    generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_NONE,
+    generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_NONE,
+    generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_NONE,
+}
+
 
 class WebSearchUtil:
     def __init__(self, api_key: str, cse_id: str) -> None:
@@ -59,4 +76,45 @@ class WebSearchUtil:
         result['abstract'] = input_text
 
         return result
+
+
+class LLMUtil:
+    def __init__(self, model_name, system_instruction=None, response_mime_type="text/plain"):
+        self.model_name = model_name
+        self.system_instruction = system_instruction
+        self.response_mime_type = response_mime_type
+        self.generation_config = {
+            "temperature": 0.0,
+            "max_output_tokens": 2048,
+            "response_mime_type": self.response_mime_type
+            }
+        self.model = GenerativeModel(model_name=self.model_name, system_instruction=self.system_instruction)
+
+    def generate_response(self, prompt: str) -> str:
+        try:
+            response = self.model.generate_content(
+                prompt,
+                generation_config=self.generation_config,
+                safety_settings=safety_settings_NONE,
+                )
+        except Exception as e:
+            print(f'error={e}')
+            return "I'm sorry, I can't answer that."
+        return response
+
+    def chat(self, message, chat_history=[]) -> str:
+        message_history = []
+        for row in chat_history:
+            input_from_user = row[0]
+            output_from_llm = row[1]
+            message_history.append(Content(role="user", parts=[Part.from_text(input_from_user)]))
+            message_history.append(Content(role="model", parts=[Part.from_text(output_from_llm)]))
+        chat = self.model.start_chat(history=message_history)
+        try:
+            response = chat.send_message(content=message, safety_settings=safety_settings_NONE)
+        except vertexai.generative_models._generative_models.ResponseBlockedError as e:
+            print(e)
+            return "I'm sorry, I can't answer that."
+        del chat_model
+        return response.text
 
