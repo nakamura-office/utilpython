34a38aeca648f20f7df04dea3c46e45a30552e1b
fllovfgv <fllovfgv@gmail.com>
2025-07-11 10:37:50 +0900

    WebSearchUtilクラスのweb_searchメソッドのdebug_mode引数のデフォルト値をTrueからFalseに変更

--- app_genaiwk/util_v2.py
+++ app_genaiwk/util_v2.py
@@ -198,7 +198,7 @@ class WebSearchUtil:
             model_name: str=model_gemini_pro25_flash,
             threshold_relevance: int=7,
             threshold_credibility: int=7,
-            debug_mode: bool=True) -> list:
+            debug_mode: bool=False) -> list:
         """
         インターネット検索結果から必要な情報を取得する関数
         引数:
